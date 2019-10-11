263187439ff3e0dedfb8c76be0d64e39fbea9cae
UVADave <dpg3k@virginia.edu>
2019-10-11 11:37:22 -0400

    Initial packaging assets

---
+++ package/scripts/entry.sh
@@ -0,0 +1,13 @@
+# set environment
+export PATH=$PATH:/usr/local/openjdk-8/bin
+
+# run application
+java -Dsolrmarc.track.solr.progress=true -Dsolrmarc.indexer.threadcount=4 \
+    -Dsolrmarc.solrj.threadcount=16 -Dsolrmarc.indexer.chunksize=5  \
+    -classpath dist/marcsqs_reader.jar:lib_solrmarc_install/solrmarc_core_3.3-6-gb6bf218-SNAPSHOT.jar \
+    org.solrmarc.driver.Boot SQSQueueDriver  \
+    -dir .  -config solr7_index.properties,rda_index_and_maps.properties,available_index.properties
+
+#
+# end of file
+#
