b00f5fce42e50e4278c73f9925ff482caa8b8d81
UVADave <dpg3k@virginia.edu>
2019-10-18 12:21:38 -0400

    Use environment variables for thread values

--- package/scripts/entry.sh
+++ package/scripts/entry.sh
@@ -2,11 +2,14 @@
 export PATH=$PATH:/usr/local/openjdk-8/bin
 
 # run application
-java -Dsolrmarc.track.solr.progress=true -Dsolrmarc.indexer.threadcount=4 \
-    -Dsolrmarc.solrj.threadcount=4 -Dsolrmarc.indexer.chunksize=10 -Dsolrmarc.track.progress=5000 \
-    -classpath dist/marcsqs_reader.jar:lib_solrmarc_install/solrmarc_core_3.3-10-gfc4f937-SNAPSHOT.jar \
-    org.solrmarc.driver.Boot SQSQueueDriver  \
-    -dir .  -config solr7_index.properties,rda_index_and_maps.properties,available_index.properties
+java -Dsolrmarc.track.solr.progress=false \
+     -Dsolrmarc.indexer.threadcount=$VIRGO4_MARC_CONVERT_INDEXER_WORKERS \
+     -Dsolrmarc.solrj.threadcount=$VIRGO4_MARC_CONVERT_SOLRJ_WORKERS \
+     -Dsolrmarc.indexer.chunksize=10 \
+     -Dsolrmarc.track.progress=5000 \
+     -classpath dist/marcsqs_reader.jar:lib_solrmarc_install/solrmarc_core_3.3-10-gfc4f937-SNAPSHOT.jar org.solrmarc.driver.Boot SQSQueueDriver  \
+     -dir . 
+     -config solr7_index.properties,rda_index_and_maps.properties,available_index.properties
 
 #
 # end of file
