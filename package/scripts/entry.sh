2e204e7030fcbee07119ded96cfab0d1da4a9fef
UVADave <dpg3k@virginia.edu>
2019-10-23 12:02:12 -0400

    Fix the startup script to referr to the vendored jarfile

--- package/scripts/entry.sh
+++ package/scripts/entry.sh
@@ -7,7 +7,7 @@ java -Dsolrmarc.track.solr.progress=false \
      -Dsolrmarc.solrj.threadcount=$VIRGO4_MARC_CONVERT_SOLRJ_WORKERS \
      -Dsolrmarc.indexer.chunksize=10 \
      -Dsolrmarc.track.progress=5000 \
-     -classpath dist/marcsqs_reader.jar:lib_solrmarc_install/solrmarc_core_3.3-10-gfc4f937-SNAPSHOT.jar org.solrmarc.driver.Boot SQSQueueDriver \
+     -classpath dist/marcsqs_reader.jar:lib_solrmarc_install/solrmarc_core_3.3-12-g1b2c032-SNAPSHOT.jar org.solrmarc.driver.Boot SQSQueueDriver \
      -dir . \
      -config solr7_index.properties,rda_index_and_maps.properties,available_index.properties
 
