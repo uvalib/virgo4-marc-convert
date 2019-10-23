9c5f8d710fb2bf66f5e7718c34441e6c2cd3707f
UVADave <dpg3k@virginia.edu>
2019-10-23 14:15:15 -0400

    Determine which solrmarc jarfile to use

--- package/scripts/entry.sh
+++ package/scripts/entry.sh
@@ -1,13 +1,16 @@
 # set environment
 export PATH=$PATH:/usr/local/openjdk-8/bin
 
+# determine the correct jarfile to use
+SOLRMARC_JAR=$(ls -t lib_solrmarc_install/solrmarc_core_* | head -1)
+
 # run application
 java -Dsolrmarc.track.solr.progress=false \
      -Dsolrmarc.indexer.threadcount=$VIRGO4_MARC_CONVERT_INDEXER_WORKERS \
      -Dsolrmarc.solrj.threadcount=$VIRGO4_MARC_CONVERT_SOLRJ_WORKERS \
      -Dsolrmarc.indexer.chunksize=10 \
      -Dsolrmarc.track.progress=5000 \
-     -classpath dist/marcsqs_reader.jar:lib_solrmarc_install/solrmarc_core_3.3-12-g1b2c032-SNAPSHOT.jar org.solrmarc.driver.Boot SQSQueueDriver \
+     -classpath dist/marcsqs_reader.jar:$SOLRMARC_JAR org.solrmarc.driver.Boot SQSQueueDriver \
      -dir . \
      -config solr7_index.properties,rda_index_and_maps.properties,available_index.properties
 
