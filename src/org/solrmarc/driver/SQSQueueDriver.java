5bb0c55c612e155776eed3d51b39345e78c11c1d
haschart <rh9ec@virginia.edu>
2019-11-22 16:12:10 -0500

    trivial change to logging message.

--- src/org/solrmarc/driver/SQSQueueDriver.java
+++ src/org/solrmarc/driver/SQSQueueDriver.java
@@ -183,7 +183,7 @@ public class SQSQueueDriver extends IndexDriver
             return;
         }
         String indexSpecString; 
-        logger.info("Received records from different datasource, re-initializing specs for "+ specSelector);
+        logger.info("Received records from different source, re-initializing specs for "+ specSelector);
         if (indexSpecMap.containsKey(specSelector))
         {
             indexSpecString = indexSpecMap.getProperty(specSelector);
